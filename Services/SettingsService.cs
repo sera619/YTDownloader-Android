4111d15588342dd38e1a85e4ce1f9f24d7051e8e
S3R43o3 <67480273+sera619@users.noreply.github.com>
2024-07-26 18:31:20 +0200

    Create SettingsService.cs

---
+++ Services/SettingsService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YTDownloaderMAUI.Services
+{
+    internal static class SettingsService
+    {
+
+        private const string CheckForUpdatesKey = "CheckForUpdatesOnStart";
+        private static readonly bool DefaultCheckForUpdates = true;
+
+        public static bool GetCheckForUpdatesOnStart()
+        {
+            return Preferences.Get(CheckForUpdatesKey, DefaultCheckForUpdates);
+        }
+
+        public static void SetCheckForUpdatesOnStart(bool value) 
+        {
+            Preferences.Set(CheckForUpdatesKey, value);
+        }
+
+    }
+}
