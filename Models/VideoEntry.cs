7873d17658709c5812f29eb33fe4d8d42b9a9ee9
S3R43o3 <67480273+sera619@users.noreply.github.com>
2024-07-26 20:38:52 +0200

    Update VideoEntry.cs

--- Models/VideoEntry.cs
+++ Models/VideoEntry.cs
@@ -11,6 +11,5 @@ namespace YTDownloaderMAUI.Models
         public required string Title { get; set; }
         public required string URL { get; set; }
         public required string Duration { get; set; }
-
     }
 }
