427bcaf4ac4d3af96b8debbbb48bd9cc50365de0
S3R43o3 <67480273+sera619@users.noreply.github.com>
2024-07-26 18:30:56 +0200

    Create SettingsPage.xaml.cs

---
+++ Pages/SettingsPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace YTDownloaderMAUI.Pages;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+
+	}
+    protected override bool OnBackButtonPressed()
+    {
+        // Optionally, you can add custom logic here
+        return base.OnBackButtonPressed(); // Return true to handle the back navigation yourself, or false to use default behavior
+    }
+}
