4c19419da1f0b6a6fd77df2f2ff5b03fedeeb02a
S3R43o3 <67480273+sera619@users.noreply.github.com>
2024-08-14 11:02:29 +0200

    Create NewDownloadPage.xaml.cs

---
+++ Pages/NewDownloadPage.xaml.cs
@@ -0,0 +1,41 @@
+using CommunityToolkit.Maui.Core.Platform;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using YTDownloaderMAUI.Models;
+using YTDownloaderMAUI.Src;
+
+namespace YTDownloaderMAUI.Pages;
+
+public partial class NewDownloadPage : ContentPage
+{
+
+    public NewDownloadPage()
+	{
+		InitializeComponent();
+        var h = Utils.GetDeviceHeight() / 3;
+        var hHalf = h * .2;
+		VideoEntryCollectionView.HeightRequest = h+hHalf;
+    }
+
+    private async void PasteUrlButton_Clicked(object sender, EventArgs e)
+    {
+        await UrlEntry.HideKeyboardAsync(CancellationToken.None);
+        UrlEntry.Unfocus();
+        string? clipboardText = await Utils.ReadFromClipboard();
+        if (string.IsNullOrEmpty(clipboardText))
+        {
+            await DisplayAlert("No content in clipboard", "No content found on your clipboard storage!", "OK");
+            return;
+        }
+        UrlEntry.Text = clipboardText;
+    }
+
+
+    private async void AddVideoToListButton_Clicked(object sender, EventArgs e)
+    {
+        await UrlEntry.HideKeyboardAsync(CancellationToken.None);
+        UrlEntry.Unfocus();
+    }
+}
